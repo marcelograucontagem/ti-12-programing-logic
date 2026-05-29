894b99f15ff7eb83b83dd79726aad4df54f66aa9
marcelograucontagem <marcelograucontagem@gmail.com>
2026-05-29 15:40:17 -0300

    Styling

--- app.js
+++ app.js
@@ -1,17 +1,8 @@
 
-const escreva = (texto)=> {
-    const body = document.querySelector('body')
+const escreva = (texto) => {
+    const body = document.querySelector('.visor')
     body.innerHTML += texto + '<br/>'
 }
 
-let total
-let valorProduto = 30
-let totalSocios = 64
-let lucroIndividual 
 
-total = valorProduto *26
-lucroIndividual =  total/totalSocios * 0.3
-
-
-escreva( 'R$ ' + lucroIndividual)
 
